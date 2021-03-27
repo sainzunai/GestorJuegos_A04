d8f783487f84e3a3c89d59385ef46df5626159dd
Unai Sainz <sainz.unai@opendeusto.es>
2021-03-27 11:54:21 +0100

    Creacion de clase JPAnelEntradaMenu

---
+++ src/main/java/es/deusto/spq/ventanas/JPanelEntradaMenu.java
@@ -0,0 +1,7 @@
+package es.deusto.spq.ventanas;
+
+import javax.swing.JPanel;
+
+public class JPanelEntradaMenu extends JPanel{
+
+}
--- src/main/java/es/deusto/spq/ventanas/VentanaPrincipal.java
+++ src/main/java/es/deusto/spq/ventanas/VentanaPrincipal.java
@@ -8,6 +8,9 @@ import javax.swing.JPanel;
 import java.awt.GridBagConstraints;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
 
 
 public class VentanaPrincipal extends JFrame{
@@ -15,18 +18,27 @@ public class VentanaPrincipal extends JFrame{
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setTitle("ORIGIN");
 		this.setSize(new Dimension(1233, 657));
-		getContentPane().setLayout(new BorderLayout());
+		getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		
-		JPanel panelMenu = new JPanel();
-		panelMenu.setBorder(new LineBorder(new Color(0, 0, 0), 3));
-		getContentPane().add(panelMenu, BorderLayout.WEST);
-		GridBagLayout gbl_panelMenu = new GridBagLayout();
-		gbl_panelMenu.columnWidths = new int[]{0};
-		gbl_panelMenu.rowHeights = new int[]{0};
-		gbl_panelMenu.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panelMenu.rowWeights = new double[]{Double.MIN_VALUE};
-		panelMenu.setLayout(gbl_panelMenu);
+		JPanel panelContenedorMenu = new JPanel();
+		panelContenedorMenu.setBackground(new Color(255, 255, 255));
+		panelContenedorMenu.setBorder(new LineBorder(new Color(0, 0, 0), 3));
+		getContentPane().add(panelContenedorMenu, BorderLayout.WEST);
+		panelContenedorMenu.setLayout(new GridLayout(4,1));
+		
+		JPanel panelEntradaMenu2 = new JPanel();
+		panelEntradaMenu2.setBorder(new LineBorder(new Color(255, 0, 255), 3));
+		panelEntradaMenu2.setBackground(new Color(192, 192, 192));
+		panelContenedorMenu.add(panelEntradaMenu2);
+		
+		JLabel lblNewLabel = new JLabel("MENU");
+		panelEntradaMenu2.add(lblNewLabel);
+		
+		JPanel panelEntradaMenu = new JPanel();
+		panelEntradaMenu.setBackground(new Color(192, 192, 192));
+		panelEntradaMenu.setBorder(new LineBorder(new Color(128, 0, 128), 3));
+		panelContenedorMenu.add(panelEntradaMenu);
 	}
 
 	public static void main(String[] args) {
