daf9d2c6ead3b275d05110e32aa38e155a8cd852
Javier Lazaro <javier.lazaro@opendeusto.es>
2021-03-28 10:25:44 +0200

    Terminada la clase VideojuegoResource

--- src/main/java/es/deusto/spq/server/VideojuegosResource.java
+++ src/main/java/es/deusto/spq/server/VideojuegosResource.java
@@ -7,8 +7,7 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Query;
 
-import com.example.User;
-
+import es.deusto.spq.VideoJuego;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -20,21 +19,20 @@ public class VideojuegosResource {
 	/**
 	 * Este método devolverá la lista completa de videojuegos que haya en la
 	 * BD al usuario. Se devolverá en formato JSON
-	 * @return
+	 * @return List<VideoJuego> Lista de videojuegos 
 	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Videojuego> getVideojuegos() {
+	public List<VideoJuego> getVideojuegos() {
 	 
-		//PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
-		//PersistenceManager pm = pmf.getPersistenceManager();
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		PersistenceManager pm = pmf.getPersistenceManager();
 		
-		//Query<Videojuego> q = pm.newQuery(Videojuego.class);
-		//q.setOrdering("");
+		Query<VideoJuego> q = pm.newQuery(VideoJuego.class);
+		q.setOrdering("nombre desc");
 		
-		//List<Videojuego> videojuegos = q.executeList();
-		//pm.close();
-		
+		List<VideoJuego> videojuegos = q.executeList();
+		pm.close();
 		return videojuegos;
 	}
 	
