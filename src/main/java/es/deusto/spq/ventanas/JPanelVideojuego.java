992c11242b4a7fb31d16248de09d3cdd2a51574c
Unai Sainz <sainz.unai@opendeusto.es>
2021-04-05 10:56:08 +0200

    añadido boton a la caratula del videojuego para poder añadir a la biblioteca

--- src/main/java/es/deusto/spq/ventanas/JPanelVideojuego.java
+++ src/main/java/es/deusto/spq/ventanas/JPanelVideojuego.java
@@ -1,9 +1,11 @@
 package es.deusto.spq.ventanas;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,14 +16,20 @@ public class JPanelVideojuego extends JPanel{
 	
 	public JPanelVideojuego(String rutaImagen) {
 		lImagen = new JLabelGraficoAjustado(rutaImagen, 13*20, 15*20);
-		this.add(lImagen);
+		this.setLayout(new BorderLayout());
+		this.add(lImagen, BorderLayout.CENTER);
+		JButton btn = new JButton("Add");
+		this.add(btn, BorderLayout.SOUTH);
 		this.setBorder(new LineBorder(Color.black));
 		this.setBackground(Color.white);
 	}
 	
 	public JPanelVideojuego() {
 		lImagen = new JLabelGraficoAjustado("", 13*20, 15*20);
-		this.add(lImagen);
+		this.setLayout(new BorderLayout());
+		this.add(lImagen, BorderLayout.CENTER);
+		JButton btn = new JButton("Add");
+		this.add(btn, BorderLayout.SOUTH);
 		this.setBorder(new LineBorder(Color.black));
 		this.setBackground(Color.white);
 	}
@@ -31,6 +39,9 @@ public class JPanelVideojuego extends JPanel{
 		panel.setSize(new Dimension(13*20, 15*20));
 		JLabel lbl = new JLabel(nombre);
 		panel.add(lbl);
+		this.setLayout(new BorderLayout());
+		JButton btn = new JButton("Add");
+		this.add(btn, BorderLayout.SOUTH);
 		this.add(panel);
 		this.setPreferredSize(new Dimension(13*20, 15*20));
 		this.setBorder(new LineBorder(Color.black));
@@ -45,7 +56,7 @@ public class JPanelVideojuego extends JPanel{
 		ventana.setVisible(true);
 		ventana.setSize(500,500);
 		ventana.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		JPanelVideojuego p = new JPanelVideojuego("FIFA20", 1);
+		JPanelVideojuego p = new JPanelVideojuego();
 		ventana.add(p);
 		ventana.revalidate();
 
