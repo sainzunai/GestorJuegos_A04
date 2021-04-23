22bf62be429803bf8cf674acaf84a63e2a42808b
Unai Sainz <sainz.unai@opendeusto.es>
2021-04-23 10:53:07 +0200

    anyadido metodo tostring al usuario

--- src/main/java/es/deusto/spq/Usuario.java
+++ src/main/java/es/deusto/spq/Usuario.java
@@ -51,4 +51,12 @@ public class Usuario implements Serializable{
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Usuario [gmail=" + gmail + ", nombre=" + nombre + ", contrasena=" + contrasena + "]";
+	}
+	
+	
 }
