a369c03782885112cdd54a22801b306175546fb1
Guillermo Villate <guvillat@opendeusto.es>
2021-04-25 14:37:00 +0200

    Actualizado los test de integracion de dao

--- src/test/java/es/deusto/spq/dao/DaoTest.java
+++ src/test/java/es/deusto/spq/dao/DaoTest.java
@@ -1,7 +1,7 @@
 package es.deusto.spq.dao;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.when;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
 
 import categories.IntegrationTest;
 import es.deusto.spq.Biblioteca;
@@ -23,18 +24,55 @@ import es.deusto.spq.dao.*;
 public class DaoTest implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-	GestorJuegos_A04DAO dao; 
-
+	private GestorJuegos_A04DAO dao= Mockito.mock(GestorJuegos_A04DAO.class);
+	Usuario usuario;
+	VideoJuego videojuego;
+	Plataforma p1;
+	Biblioteca biblioteca;
 	@Before
 	public void setup() {
 		
-	dao = new GestorJuegos_A04DAO();
-		
-		
+		usuario = new Usuario("themrguiller@gmail.com", "12345", "Guillermo");
+		p1 = new Plataforma("PS4", "1");
+		videojuego = new VideoJuego("Battlefield ", "2", "EA", 18, p1);
+		biblioteca = new Biblioteca();
 	}
 	@Test
-	public void testIntroducir() {
+	public void getUsuario() {
+		usuario.setBiblioteca(biblioteca);
+		biblioteca.setUser(usuario);
+		dao.introducirObjeto(usuario);
+		when(dao.getUsuario("themrguiller@gmail.com")).thenReturn(usuario);
+		assertEquals("themrguiller@gmail.com", dao.getUsuario("themrguiller@gmail.com").getGmail());
 	
+	
+	}
+	@Test
+	public void getVideojuego() {
+		p1.addVideoJuego(videojuego);
+		dao.introducirObjeto(p1);
+		when(dao.getVideojuego(videojuego.getId())).thenReturn(videojuego);
+		assertEquals("2", dao.getVideojuego(videojuego.getId()).getId());
+	}
+	@Test
+	public void getBiblioteca() {
+		usuario.setBiblioteca(biblioteca);
+		biblioteca.setUser(usuario);
+		when(dao.getBiblioteca_Usuario(usuario)).thenReturn(biblioteca);
+		assertEquals(null, dao.getBiblioteca_Usuario(usuario).getId());//Esto se debe a que la base de datos es la cual le da un id a la biblioteca
+	}
+	@Test
+	public void getAllVideojuego() {
+		List<VideoJuego> lista= new ArrayList<>();
+		lista.add(videojuego);
+		when(dao.getAllVideojuegos()).thenReturn(lista);
+		assertEquals(lista, dao.getAllVideojuegos());
+	}
+	@Test
+	public void UpdateBiblio_Video() {
+		//when(dao.updateBiblioteca_Videojuego(biblioteca, videojuego));
+	}
+	/*
 	Usuario user = new Usuario("themrguiller@gmail.com", "Hackeado","Guillermo");
 	Plataforma p1 = new Plataforma("PS4", "1");
 	VideoJuego v1 = new VideoJuego("Battlefield V", "1", "EA", 18, p1); 
@@ -82,8 +120,8 @@ public class DaoTest implements Serializable{
 	System.out.println(user2.getBiblioteca().getListaJuegos());
 	dao.updateBiblioteca(user2.getBiblioteca());
 	dao.updateBiblioteca(prueba.getBiblioteca());
-	
-	}
+	*/
+	 
 	
 
 	
