23f6907719f7ace3b81b2783680dfeddf67ce781
Javier Lazaro <javier.lazaro@opendeusto.es>
2021-04-27 15:58:52 +0200

    Acabado el test de la clase Biblioteca con Mockito

--- src/test/java/es/deusto/spq/BibliotecaTest.java
+++ src/test/java/es/deusto/spq/BibliotecaTest.java
@@ -1,126 +1,173 @@
 package es.deusto.spq;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import es.deusto.spq.dao.GestorJuegos_A04DAO;
 
 public class BibliotecaTest {
 	
 	Biblioteca b; 
 	
-	VideoJuego v1;
+	//EJEMPLO DE USO DE MOCKITO 
+	private VideoJuego videojuego = Mockito.mock(VideoJuego.class);
+	private Usuario user = Mockito.mock(Usuario.class);
+	private ArrayList<VideoJuego> listaVideojuegos;
 	
-	VideoJuego v2;
-	
-	VideoJuego v3;
-	
-	VideoJuego v4;
-	
-	ArrayList<VideoJuego> v; 
+//	VideoJuego v1;
+//	
+//	VideoJuego v2;
+//	
+//	VideoJuego v3;
+//	
+//	VideoJuego v4;
+//	
+//	ArrayList<VideoJuego> v; 
 
 	
 	
-    @Before                                         
+    @Before                                      
     public void setUp() throws Exception {
     	b = new Biblioteca();
-    	v1 = new VideoJuego("Battlefield","1","EA",18,new Plataforma());
-    	v2 = new VideoJuego("Battlefield","2","EA",18,new Plataforma());
-    	v3 = new VideoJuego("Battlefield","3","EA",18,new Plataforma());
-    	v4 = new VideoJuego("Battlefield","4","EA",18,new Plataforma());
+    	//b.setId("1"); //Para el test de getID
+    	//listaVideojuegos = new ArrayList<>();
+    	//listaVideojuegos.add(videojuego);
     	
-    	b.addJuego(v1);
-    	b.addJuego(v2);
-    	b.addJuego(v3);
+//    	v1 = new VideoJuego("Battlefield","1","EA",18,new Plataforma());
+//    	v2 = new VideoJuego("Battlefield","2","EA",18,new Plataforma());
+//    	v3 = new VideoJuego("Battlefield","3","EA",18,new Plataforma());
+//    	v4 = new VideoJuego("Battlefield","4","EA",18,new Plataforma());
     	
-    	
+    	//b.addJuego(v1);
+    	//b.addJuego(v2);
+    	//b.addJuego(v3);
     	//b.addJuego(v4);
     	
-    	v = new ArrayList<>(); 
-    	
-    	v.add(v1);
-    	v.add(v2);
-    	v.add(v3);
+//    	v = new ArrayList<>(); 
+//    	
+//    	v.add(v1);
+//    	v.add(v2);
+//    	v.add(v3);
     	
 
     }
 
+    //EJEMPLO DE USO DE MOCKITO --> Añado el mock, lo elimino y veo que el numero de juegos es el mismo
 	@Test
 	public void testRemJuego() {
-		
-		b.remJuego(v1);
-		
-		assertEquals(2, b.numeroDeJuegos());
+		b.addJuego(videojuego);
+		b.remJuego(videojuego);
+		assertEquals(0, b.numeroDeJuegos());
 	}
 
+	//EJEMPLO DE USO DE MOCKITO --> Añado el objeto mock y le digo cómo se tiene que comportar
 	@Test
 	public void testAddJuego() {
-		
-		b.addJuego(v4);
-		assertEquals(4, b.numeroDeJuegos());		
-		
+		b.addJuego(videojuego);
+		when(videojuego.getId()).thenReturn("4");
+		assertEquals(1, b.numeroDeJuegos());			
+	}
+	
+	@Test
+	public void testAddJuegoRepetido() {
+		b.addJuego(videojuego);
+		when(videojuego.getId()).thenReturn("4");
+		b.addJuego(videojuego);
+		assertEquals(1, b.numeroDeJuegos());			
+	}
+	
+	@Test
+	public void testCrearBiblioteca() {
+		listaVideojuegos = new ArrayList<>();
+		b = new Biblioteca(listaVideojuegos);
+		assertNotNull(b);			
 	}
 	
 	@Test
 	public void testGetId() {
-		
+		assertNull(b.getId());		
 	}
 	
 	@Test
 	public void testSetId() {
 		b.setId("HOLA");
-		
-		assertEquals("HOLA", b.getId());		
-		
-		
+		assertEquals("HOLA", b.getId());	
 	}
 	
+	//Nada nuevo con Mockito, solo que a la lista le
 	@Test	
 	public void testGetListaJuegos() {
-		
-		assertEquals(3, b.getListaJuegos().size());
-		
+		b.addJuego(videojuego);
+		assertEquals(1, b.getListaJuegos().size());
 	}
 	
 	@Test
 	public void testSetListaJuegos() {
-		
-		b.setListaJuegos(v);
+		listaVideojuegos = new ArrayList<>();
+		listaVideojuegos.add(videojuego);
+		b.setListaJuegos(listaVideojuegos);
 		assertNotNull(b.getListaJuegos());
-		
-		
 	}
 	
 	@Test	
 	public void testGetUser() {
-		
 		assertNull(b.getUser());
-		
 	}
 	
 	@Test
 	public void testSetUser() {
-		
-		b.setUser(new Usuario("HOLA", "HOLA", "PRUEBA"));
-		
+		b.setUser(user);
 		assertNotNull(b.getUser());
-		
 	}
 	
 	@Test
 	public void testNumeroDeJuegos() {
-		
-		
-		assertEquals(3, b.numeroDeJuegos());
-		
+		b.addJuego(videojuego);	
+		assertEquals(1, b.numeroDeJuegos());	
 	}
 	
+	@Test
+	public void testEqualsBien() {
+		assertEquals(b, b);	
+	}
 	
+	@Test
+	public void testEqualsMal() {
+		assertNotEquals(b, videojuego);	
+	}
 	
+	@Test
+	public void testEqualsNull() {
+		assertNotEquals(b, null);	
+	}
+	
+	@Test
+	public void testEqualsDistintoIdNull() {
+		Biblioteca b1 = new Biblioteca();
+		b1.setId("1");
+		assertNotEquals(b, b1);
+	}
 
+	@Test
+	public void testEqualsDistintoIdNotNull() {
+		Biblioteca b1 = new Biblioteca();
+		b1.setId("1");
+		b.setId("2");
+		assertNotEquals(b, b1);
+	}
 	
-
-	
+	@Test
+	public void testEqualsMismoId() {
+		Biblioteca b1 = new Biblioteca();
+		b1.setId("1");
+		b.setId("1");
+		assertEquals(b, b1);
+	}	
 }
