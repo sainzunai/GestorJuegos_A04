a5efb1940979bbd0e0bbd58b9a0ca25093c3e5c0
Unai Sainz <sainz.unai@opendeusto.es>
2021-05-12 09:18:37 +0200

    Test ejecucion ventana con estrellas para rating

---
+++ src/main/java/es/deusto/spq/ventanas/Test.java
@@ -0,0 +1,26 @@
+package es.deusto.spq.ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import es.deusto.spq.ventanas.recursos_test.StarRater;
+
+public class Test {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		StarRater starRater = new StarRater(5, 2, 1);
+		starRater.addStarListener(new StarRater.StarListener() {
+
+			public void handleSelection(int selection) {
+				System.out.println(selection);
+			}
+		});
+		panel.add(starRater);
+		frame.getContentPane().add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
