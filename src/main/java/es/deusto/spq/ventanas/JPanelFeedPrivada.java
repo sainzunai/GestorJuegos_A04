03dece17f622ee1886b9f608222573837c8a3b58
Unai Sainz <sainz.unai@opendeusto.es>
2021-05-18 16:14:06 +0200

    formateo y upload de fotos para la ventana feed

--- src/main/java/es/deusto/spq/ventanas/JPanelFeedPrivada.java
+++ src/main/java/es/deusto/spq/ventanas/JPanelFeedPrivada.java
@@ -32,16 +32,16 @@ public class JPanelFeedPrivada extends JPanel{
 			}
 		});
 		setLayout(null);
-		
+
 		this.add(starRater);
-		
+
 		JLabel lTitulo = new JLabel("Bienvenido " + u.getNombre() + " a tu feed privada de " + v.getNombre());
 		lTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		lTitulo.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lTitulo.setBounds(221, 10, 643, 39);
 		add(lTitulo);
-		
-		
+
+
 		JTextArea taSubtitulo = new JTextArea();
 		taSubtitulo.setEditable(false);
 		taSubtitulo.setLineWrap(true);
@@ -50,40 +50,40 @@ public class JPanelFeedPrivada extends JPanel{
 		taSubtitulo.setBounds(27, 58, 927, 39);
 		add(taSubtitulo);
 		taSubtitulo.setOpaque(false);
-		
-//		if(u != null && v != null) {
-			JLabelGraficoAjustado lCaratula = new JLabelGraficoAjustado(v.getCaratula(), 300, 500);
-			add(lCaratula);
-			lCaratula.setBounds(37, 108, 300, 335);
-			lCaratula.setBorder(LineBorder.createBlackLineBorder());
-			
-			JLabel lblNewLabel = new JLabel("Nombre: " + v.getNombre());
-			lblNewLabel.setBounds(365, 130, 200, 13);
-			add(lblNewLabel);
-			
-			JLabel lblNewLabel_1 = new JLabel("Compania: " + v.getCompania());
-			lblNewLabel_1.setBounds(365, 153, 200, 13);
-			add(lblNewLabel_1);
-			
-			JLabel lblNewLabel_1_1 = new JLabel("Edad recomendada: " + v.getEdadRecomendada());
-			lblNewLabel_1_1.setBounds(365, 176, 200, 13);
-			add(lblNewLabel_1_1);
-			
-			JLabel lblNewLabel_1_2 = new JLabel("Identificador: " + v.getId());
-			lblNewLabel_1_2.setBounds(365, 199, 200, 13);
-			add(lblNewLabel_1_2);
-			
-			JLabel lblNewLabel_1_3 = new JLabel("Mi puntuacion: ");
-			lblNewLabel_1_3.setBounds(365, 222, 200, 13);
-			add(lblNewLabel_1_3);
-			
-			JPanel pNovedades = new JPanel();
-			pNovedades.setBorder(new TitledBorder(new LineBorder(null), "Mis ultimas novedades", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-			pNovedades.setBounds(531, 107, 448, 325);
-			add(pNovedades);
-			
-//		}
-		
+
+		//		if(u != null && v != null) {
+		JLabelGraficoAjustado lCaratula = new JLabelGraficoAjustado(v.getCaratula(), 300, 500);
+		add(lCaratula);
+		lCaratula.setBounds(37, 108, 300, 335);
+		lCaratula.setBorder(LineBorder.createBlackLineBorder());
+
+		JLabel lblNewLabel = new JLabel("Nombre: " + v.getNombre());
+		lblNewLabel.setBounds(365, 130, 200, 13);
+		add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Compania: " + v.getCompania());
+		lblNewLabel_1.setBounds(365, 153, 200, 13);
+		add(lblNewLabel_1);
+
+		JLabel lblNewLabel_1_1 = new JLabel("Edad recomendada: " + v.getEdadRecomendada());
+		lblNewLabel_1_1.setBounds(365, 176, 200, 13);
+		add(lblNewLabel_1_1);
+
+		JLabel lblNewLabel_1_2 = new JLabel("Identificador: " + v.getId());
+		lblNewLabel_1_2.setBounds(365, 199, 200, 13);
+		add(lblNewLabel_1_2);
+
+		JLabel lblNewLabel_1_3 = new JLabel("Mi puntuacion: ");
+		lblNewLabel_1_3.setBounds(365, 222, 200, 13);
+		add(lblNewLabel_1_3);
+
+		JPanel pNovedades = new JPanel();
+		pNovedades.setBorder(new TitledBorder(new LineBorder(null), "Mis ultimas novedades", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pNovedades.setBounds(531, 107, 448, 325);
+		add(pNovedades);
+
+		//		}
+
 	}
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
