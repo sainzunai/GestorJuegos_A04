c45bfd0d6d69e6f460d63f855dfd541e61a3b3e2
Javier Lazaro <javier.lazaro@opendeusto.es>
2021-05-22 10:21:10 +0200

    Creados los test iniciales vacios para las nuevas clases

---
+++ src/test/java/es/deusto/spq/ComentarioTest.java
@@ -0,0 +1,24 @@
+package es.deusto.spq;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ComentarioTest {
+	
+	private Comentario c;
+	
+	@Before
+	public void setUp() throws Exception{
+		
+		c = new Comentario();
+	
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/es/deusto/spq/ReviewTest.java
@@ -0,0 +1,24 @@
+package es.deusto.spq;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReviewTest {
+	
+	private Review r;
+	
+	@Before
+	public void setUp() throws Exception{
+		
+		r = new Review();
+	
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
